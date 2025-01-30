39287bcbee4d3736ad7ecd5daa3867f42d58f5db
otkamal <okamal@duck.com>
2025-01-29 21:38:03 -0800

    added clock and capped fps at 60

--- main.py
+++ main.py
@@ -9,13 +9,19 @@ def main():
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(constants.COLOR_BLACK)
         pygame.display.flip()
+        dt = clock.tick(constants.MAX_FPS)
+        # convert dt from milliseconds to seconds
+        dt /= 1000
+
 
 if __name__ == "__main__":
     main()
