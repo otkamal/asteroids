ab1ce93c95c4eeb1708e4568f6f55af2d3a2b783
otkamal <okamal@duck.com>
2025-01-29 22:22:41 -0800

    moved player updates to groups

--- main.py
+++ main.py
@@ -5,21 +5,31 @@ import player
 def main():
 
     print("Starting asteroids!")
+     
     pygame.init()
 
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
+
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    player.Player.containers = (updatable, drawable)
     p = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
-    dt = 0
+    
 
+
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                print(f"Ending asteroids")
                 return
         screen.fill(constants.COLOR_BLACK)
-        p.draw(screen)
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
         dt = clock.tick(constants.MAX_FPS)
         # convert dt from milliseconds to seconds
--- player.py
+++ player.py
@@ -23,3 +23,21 @@ class Player(circleshape.CircleShape):
             self.triangle(),
             constants.PLAYER_LINE_WIDTH
         )
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * constants.PLAYER_SPEED * dt
+    
+    def rotate(self, dt):
+        self.rotation += constants.PLAYER_TURN_SPEED * dt
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
