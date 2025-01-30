0eea9ff23495f8128660aaca517ebbbcd069d6f4
otkamal <okamal@duck.com>
2025-01-29 21:53:38 -0800

    the player exists now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import constants
+import player
 
 def main():
 
@@ -10,6 +11,7 @@ def main():
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    p = player.Player(constants.SCREEN_WIDTH / 2, constants.SCREEN_HEIGHT / 2)
     dt = 0
 
     while True:
@@ -17,6 +19,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill(constants.COLOR_BLACK)
+        p.draw(screen)
         pygame.display.flip()
         dt = clock.tick(constants.MAX_FPS)
         # convert dt from milliseconds to seconds
